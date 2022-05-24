380809e0b9efe7a6ac21675f24a721710a9c58b5
edstokalskiy <edstokalskiy@gmail.com>
2022-05-24 14:07:25 +0300

    renamed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# zsu
+# users
 
 ## Build Setup
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ZSU',
+    title: 'users',
     htmlAttrs: {
       lang: 'en'
     },
